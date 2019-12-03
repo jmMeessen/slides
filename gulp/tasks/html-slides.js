2614929b78d7f7925e7936bcf3b05a2b66fe1143
dduportal <1522731+dduportal@users.noreply.github.com>
2019-12-03 17:19:28 +0100

    Enable revealjs plugins
    Signed-off-by: dduportal <1522731+dduportal@users.noreply.github.com>

--- gulp/tasks/html-slides.js
+++ gulp/tasks/html-slides.js
@@ -14,6 +14,8 @@ module.exports = function (gulp, plugins, current_config) {
                             'docinfosPath': current_config.docinfosPath,
                             'presentationUrl': process.env.PRESENTATION_URL,
                             'repositoryUrl': process.env.REPOSITORY_URL,
+                            'revealjs_plugins': current_config.revealJSPluginList,
+                            'revealjs_plugins_configuration': current_config.scriptsSrcPath + '/revealjs-plugins-config.js'
                         },
                         to_dir: current_config.distDir,
                     }
